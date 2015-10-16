9b4045e85e23835073f8faa5c800847f915172a3
gianordoli <gianordoligabriel@yahoo.com.br>
2015-10-15 22:23:04 -0400

    fixing accel

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -79,8 +79,10 @@ if (!window.DeviceMotionEvent) {
     document.getElementById('interval').innerHTML = event.interval;
 
     xyz = {
-      x: Math.round(event.acceleration.x),
-      y: Math.round(event.acceleration.z)
+      x: Math.round(event.accelerationIncludingGravity.x),
+      y: Math.round(event.accelerationIncludingGravity.z) + 10
+      // x: Math.round(event.acceleration.x),
+      // y: Math.round(event.acceleration.z)
       // x: Math.round(event.accelerationIncludingGravity.x),
       // y: Math.round(event.accelerationIncludingGravity.y),
       // z: Math.round(event.accelerationIncludingGravity.z)
