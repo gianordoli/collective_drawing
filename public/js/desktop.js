22978f3fb660904b6d15580eca5cc49b46126252
gianordoli <gianordoligabriel@yahoo.com.br>
2015-10-15 22:08:37 -0400

    modular pattern

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -51,9 +51,67 @@ app.main = (function() {
     // });     
   };
 
+  var canvas, context;
+  var width, height;
+  var posX, posY, speedX, speedY, radius;
+
+  var canvasSetup = function(){
+    canvas = document.getElementById('maze');
+    context = canvas.getContext ('2d');
+    if (canvas.getContext){
+      width = window.innerWidth;
+      height = window.innerHeight;
+      canvas.style.width = width + 'px';
+      canvas.style.height = height + 'px';
+      posX = 20;
+      posY = 79;
+      speedX = 0;
+      speedY = 0;
+      radius = 12;
+    }else{
+      document.write ("Your browser doesn't support canvas :S");
+    }
+  };
+
+  function updatePosition(data) {
+    
+    speedX = data.x * 0.1;
+    speedY = data.y * 0.1;
+    posX += speedX;
+    posY += speedY;
+
+    if (posX < 0){
+      posX = 0;
+    }else if(posX > width){
+      posX = width;
+    }
+
+    if(posY < 0){
+      posxY = 0;
+    }else if(posY > height) {  
+      posY = height;
+    }
+
+    draw();
+  };
+
+  function draw() {
+
+    // Backgronud
+    context.fillStyle = 'rgba(255, 255, 255, .05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Circle
+    context.beginPath();
+    context.arc(posX, posY, radius, 0, 2*Math.PI);
+    context.fillStyle = 'rgba(29, 104, 255, 1)';
+    context.fill();
+  };
+
   var init = function(){
     console.log('Initializing app.');
     socketSetup();
+    canvasSetup();
   };
 
   return {
@@ -63,57 +121,3 @@ app.main = (function() {
 })();
 
 window.addEventListener('DOMContentLoaded', app.main.init);
-
-var canvas = document.getElementById('maze');
-var context = canvas.getContext ('2d');
-var width = window.innerWidth;
-var height = window.innerHeight;
-canvas.style.width = width + 'px';
-canvas.style.height = height + 'px';
-
-//positions
-var posX = 20;
-var posY = 79;
-var speedX = 0;
-var speedY = 0;
-var radius = 12;
-var direction;
-
-function updatePosition(data) {
-  speedX = data.x * 0.1;
-  speedY = data.y * 0.1;
-  posX += speedX;
-  posY += speedY;
-  if (posX < 0){
-    posX = 0;
-  }else if(posX > width){
-    posX = width;
-  }
-
-  if(posY < 0){
-    posxY = 0;
-  }else if(posY > height) {  
-    posY = height;
-  }
-}
-
-function draw() {
-
-  if (canvas.getContext) { 
-
-    context.fillStyle = 'rgba(255, 255, 255, .05)';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    //colors
-    var color = 'rgba(250, 146, 146, 1)';
-    var colorCirlce = 'rgba(29, 104, 255, 1)';
-
-  /// cirlce draw
-      context.beginPath();
-      context.arc(posX, posY, radius, 0, 2*Math.PI);
-      context.fillStyle = colorCirlce;
-      context.fill();
-  }else{
-    document.write ("Your browser doesn't support canvas :S");
-  }
-};
