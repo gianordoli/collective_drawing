6a33ea2cb512ee4a720f7883bc9376560b4acaa4
gianordoli <gianordoligabriel@yahoo.com.br>
2015-10-17 12:10:32 -0400

    cleaning up

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,61 +1,22 @@
 var app = app || {};
 
 app.main = (function() {
-  console.log('Your code starts here!');
 
   var socket;
+  var canvas, context;
+  var width, height;
 
   // Initializing socket and adding listener functions
   var socketSetup = function(callback){
-    
-    // Connect
-      socket = io.connect();
 
-      socket.on('render', function(data) {
-        console.log(data);
-        // updatePosition(data);
-        draw(data);
-      });
+    socket = io.connect();
 
-    // Listeners
-    // socket.on('welcome', function(data){
-    //   // console.log(data.msg);
-    //   // console.log(data.users);
-    //   for(var id in data.users){
-    //     addBall({
-    //       id: id,
-    //           color: data.users[id]['color'],
-    //           top: data.users[id]['top'],
-    //           left: data.users[id]['left']          
-    //     });
-    //   }
-    // });
-
-    // socket.on('add-ball', function(data){
-    //   addBall(data);
-    // });
-    
-    // socket.on('remove-ball', function(data){
-    //   if(ballExists(data.id)){
-    //     removeBall(data);
-    //   }
-    // });
-
-    // // Listen again, this time to render
-    // socket.on('render', function(data){
-    //   // console.log(data);
-    //   // If a ball with this ID hasn't been rendered yet, let's add it
-    //   if(!ballExists(data.id)){
-    //     addBall(data);
-    //   }
-    //   moveBall(data);
-    // });     
+    socket.on('render', function(data) {
+      // console.log(data);
+      // updatePosition(data);
+      draw(data);
+    });
   };
-
-  var canvas, context;
-  var width, height;
-  var balls = {};
-  // var posX, posY, speedX, speedY, radius;
 
   var canvasSetup = function(){
     canvas = document.getElementById('maze');
@@ -65,45 +26,10 @@ app.main = (function() {
       height = window.innerHeight;
       canvas.style.width = width + 'px';
       canvas.style.height = height + 'px';
-      // posX = 50;
-      // posY = 50;
-      // speedX = 0;
-      // speedY = 0;
-      // radius = 5;
     }else{
       document.write ("Your browser doesn't support canvas :S");
     }
   };
-
-  // var addBall = function(data){
-  //   console.log('Appending a new ball: ' + data.id);
-  //   var ball = $('<div class="ball" id="' + data.id + '"></div>')
-  //         .css({
-  //           'background-color': data.color,
-  //           'top': data.top,
-  //           'left': data.left
-  //         });
-  //   $('body').append(ball);
-  // };
-
-  // var removeBall = function(data){
-  //   console.log('Called removeBall for ' + data);
-  //   $('#' + data.id).remove();
-  // };
-
-  // var moveBall = function(data){
-  //   $('#' + data.id).css({
-  //     'top': data.top,
-  //     'left': data.left
-  //   });
-  // };  
-
-  // function updatePosition(data) {
-
-
-
-  //   draw();
-  // };
 
   function draw(data) {
 
@@ -113,8 +39,6 @@ app.main = (function() {
 
     // Circle
     context.beginPath();
-    // context.arc(posX, posY, radius, 0, 2*Math.PI);
-    // context.fillStyle = 'rgba(29, 104, 255, 1)';
     context.arc(data['pos']['x'], data['pos']['y'], 5, 0, 2*Math.PI);
     context.fillStyle = data['color'];
     context.fill();
--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,108 +1,76 @@
-// Socket.io connection
-var socket = io.connect(); //socket to listen to app
+var app = app || {};
 
-var xyz = {};
+app.main = (function() {
 
-// Assigning function to the 'start' event on that socket
-socket.on('start', function(data) { //when we get data from socket
-  console.log('User is ' + data.user);
-  console.log('Date is ' + data.date);
-});
+  var socket;
 
+  // Initializing socket and adding listener functions
+  var socketSetup = function(callback){
+      socket = io.connect();
+      // Assigning function to the 'start' event on that socket
+      socket.on('start', function(data) { //when we get data from socket
+        console.log('User is ' + data.user);
+        console.log('Date is ' + data.date);
+      });      
+  };
 
-// check if DeviceOrientationEvent is supported
-if (!window.DeviceOrientationEvent) {
-  document.getElementById('do-unsupported').classList.remove('hidden');
-} else {
-  document.getElementById('do-info').classList.remove('hidden');
+  var attachEvents = function(){
 
-  //listen for event and handle DeviceOrientationEvent object
-  window.addEventListener('deviceorientation', function(event) {
+    // check if DeviceOrientationEvent is supported
+    if (!window.DeviceOrientationEvent) {
+      document.getElementById('do-unsupported').classList.remove('hidden');
+    } else {
+      document.getElementById('do-info').classList.remove('hidden');
 
-    // gamma is the left-to-right tilt in degrees, where right is positive
-    var tiltLeftToRight = event.gamma;
-    // beta is the front-to-back tilt in degrees, where front is positive
-    var tiltFrontToBack = event.beta;
-    // alpha is the compass direction the device is facing in degrees
-    var direction = event.alpha;
+      //listen for event and handle DeviceOrientationEvent object
+      window.addEventListener('deviceorientation', function(event) {
 
-    // rotate image using CSS3 transform
-    var cube = document.getElementById('cube');
-    cube.style.webkitTransform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
-    cube.style.MozTransform = 'rotate(' + tiltLeftToRight + 'deg)';
-    cube.style.transform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
+        // gamma is the left-to-right tilt in degrees, where right is positive
+        var tiltLeftToRight = event.gamma;
+        // beta is the front-to-back tilt in degrees, where front is positive
+        var tiltFrontToBack = event.beta;
+        // alpha is the compass direction the device is facing in degrees
+        var direction = event.alpha;
 
-    // set HTML content = tilt OR direction degree (rounded to nearest integer)
-    document.getElementById('doTiltFrontToBack').innerHTML = Math.round(tiltFrontToBack);
-    document.getElementById('doTiltLeftToRight').innerHTML = Math.round(tiltLeftToRight);
-    document.getElementById('doDirection').innerHTML = Math.round(direction);
+        // rotate image using CSS3 transform
+        var cube = document.getElementById('cube');
+        cube.style.webkitTransform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
+        cube.style.MozTransform = 'rotate(' + tiltLeftToRight + 'deg)';
+        cube.style.transform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
 
-    document.getElementById('is-absolute').innerHTML = event.absolute ? "true" : "false";
- 
-    xyz = {
-      // x: Math.round(tiltLeftToRight),
-      // y: Math.round(tiltFrontToBack),
-      // z: Math.round(direction)
-      x: Math.round(direction),
-      y: -Math.round(tiltFrontToBack)
-    };
-    socket.emit('coordinates', xyz);
+        // set HTML content = tilt OR direction degree (rounded to nearest integer)
+        document.getElementById('doTiltFrontToBack').innerHTML = Math.round(tiltFrontToBack);
+        document.getElementById('doTiltLeftToRight').innerHTML = Math.round(tiltLeftToRight);
+        document.getElementById('doDirection').innerHTML = Math.round(direction);
 
-  });
-}
+        document.getElementById('is-absolute').innerHTML = event.absolute ? "true" : "false";
+     
+        // coordinates = {
+        //   // x: Math.round(tiltLeftToRight),
+        //   // y: Math.round(tiltFrontToBack),
+        //   // z: Math.round(direction)
+        //   x: Math.round(direction),
+        //   y: -Math.round(tiltFrontToBack)
+        // };
+        socket.emit('coordinates', {
+          x: Math.round(direction),
+          y: -Math.round(tiltFrontToBack)
+        });
 
+      });
+    }
+  }
 
-// if (!window.DeviceMotionEvent) {
-//   document.getElementById('dm-unsupported').classList.remove('hidden');
-// } else {
-//   document.getElementById('dm-info').classList.remove('hidden');
+  var init = function(){
+    console.log('Initializing app.');
+    socketSetup();
+    attachEvents();
+  };
 
-//   window.addEventListener('devicemotion', function(event) {
+  return {
+    init: init
+  };
 
-//     // grab acceleration from the results
-//     document.getElementById('acceleration-x').innerHTML = Math.round(event.acceleration.x);
-//     document.getElementById('acceleration-y').innerHTML = Math.round(event.acceleration.y);
-//     document.getElementById('acceleration-z').innerHTML = Math.round(event.acceleration.z);
+})();
 
-//     // grab acceleration incl. gravitiy from the results
-//     document.getElementById('acceleration-including-gravity-x').innerHTML =
-//       Math.round(event.accelerationIncludingGravity.x);
-//     document.getElementById('acceleration-including-gravity-y').innerHTML =
-//       Math.round(event.accelerationIncludingGravity.y);
-//     document.getElementById('acceleration-including-gravity-z').innerHTML =
-//       Math.round(event.accelerationIncludingGravity.z);
-
-//     // grab rotation rate from the results
-//     document.getElementById('rotation-rate-beta').innerHTML = Math.round(event.rotationRate.beta);
-//     document.getElementById('rotation-rate-gamma').innerHTML = Math.round(event.rotationRate.gamma);
-//     document.getElementById('rotation-rate-alpha').innerHTML = Math.round(event.rotationRate.alpha);
-
-//     // grab refresh interval from the results
-//     document.getElementById('interval').innerHTML = event.interval;
-
-//     xyz = {
-//       x: Math.round(event.accelerationIncludingGravity.x),
-//       y: Math.round(event.accelerationIncludingGravity.z) + 10
-//       // x: Math.round(event.acceleration.x),
-//       // y: Math.round(event.acceleration.z)
-//       // x: Math.round(event.accelerationIncludingGravity.x),
-//       // y: Math.round(event.accelerationIncludingGravity.y),
-//       // z: Math.round(event.accelerationIncludingGravity.z)
-//       // x: Math.round(event.tiltLeftToRight),
-//       // y: Math.round(event.tiltFrontToBack)
-//       // z: Math.round(event.acceleration.z)      
-//     };
-//     socket.emit('coordinates', xyz);
-    
-//   });
-// }
-
-/*
-if (!('oncompassneedscalibration' in window)) {
-  document.getElementById('cnc-unsupported').classList.remove('hidden');
-} else {
-  window.addEventListener('compassneedscalibration', function(event) {
-    alert('Compass needs calibrating! Wave your device in a figure-eight motion');
-  });
-}
-*/
+window.addEventListener('DOMContentLoaded', app.main.init);  
