23f327eebd26b9bec62c0668e3f4174441df94d3
gianordoli <gianordoligabriel@yahoo.com.br>
2015-10-17 13:41:15 -0400

    bug

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -50,17 +50,25 @@ app.main = (function() {
   }
 
   var displayOrientation = function(){
-      // rotate image using CSS3 transform
-      var cube = document.getElementById('cube');
-      cube.style.webkitTransform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
-      cube.style.MozTransform = 'rotate(' + tiltLeftToRight + 'deg)';
-      cube.style.transform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
+    // rotate image using CSS3 transform
+    var cube = document.getElementById('cube');
 
-      // set HTML content = tilt OR direction degree (rounded to nearest integer)
-      document.getElementById('doTiltFrontToBack').innerHTML = Math.round(tiltFrontToBack);
-      document.getElementById('doTiltLeftToRight').innerHTML = Math.round(tiltLeftToRight);
-      document.getElementById('doDirection').innerHTML = Math.round(direction);
-      document.getElementById('is-absolute').innerHTML = event.absolute ? "true" : "false";
+    // gamma is the left-to-right tilt in degrees, where right is positive
+    var tiltLeftToRight = event.gamma;
+    // beta is the front-to-back tilt in degrees, where front is positive
+    var tiltFrontToBack = event.beta;
+    // alpha is the compass direction the device is facing in degrees
+    var direction = event.alpha;
+
+    cube.style.webkitTransform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
+    cube.style.MozTransform = 'rotate(' + tiltLeftToRight + 'deg)';
+    cube.style.transform = 'rotate(' + tiltLeftToRight + 'deg) rotate3d(1,0,0, ' + (tiltFrontToBack * -1) + 'deg)';
+
+    // set HTML content = tilt OR direction degree (rounded to nearest integer)
+    document.getElementById('doTiltFrontToBack').innerHTML = Math.round(tiltFrontToBack);
+    document.getElementById('doTiltLeftToRight').innerHTML = Math.round(tiltLeftToRight);
+    document.getElementById('doDirection').innerHTML = Math.round(direction);
+    document.getElementById('is-absolute').innerHTML = event.absolute ? "true" : "false";
   }
 
   var init = function(){
