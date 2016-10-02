1c3f1af5cfaab2ad489d019698ead047f3be57ca
gianordoli <gianordoligabriel@yahoo.com.br>
2016-10-02 17:20:16 -0400

    isCalibrated fix

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -69,6 +69,7 @@ app.main = (function() {
     if(touches === 3) {
       calibration["alpha"]["max"] = orientation.x;
       socket.emit('new-calibration', calibration);
+      isCalibrated = true;
     }    
   }
 
