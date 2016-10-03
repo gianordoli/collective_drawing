e8f0e8895e72c35d7ddfd210b94275774deefae1
Gabriel Gianordoli <gianordoligabriel@gmail.com>
2016-10-03 14:46:18 -0400

    mobile artifacts

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -43,7 +43,7 @@ app.main = (function() {
       //listen for event and handle DeviceOrientationEvent object
       window.addEventListener('deviceorientation', function(event) {
         orientation = getOrientation(event);
-        displayOrientation(event);
+        // displayOrientation(event);
         if(isCalibrated){
           emitOrientation();
         }
